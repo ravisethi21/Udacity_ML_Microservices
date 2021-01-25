7cc6f55e351d84edc80e7ee5f43aae69dfb7ab87
sumitarora02 <arorasumit0205@gmail.com>
2021-01-26 00:39:31 +0530

    Initial commit

---
+++ utils/run_kubernetes.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=sumitarora02/project-microservices-udacity
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run project-microservices-udacity\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=ml-udacity
+
+# Wait to pod status will be ready
+kubectl wait pod/project-microservices-udacity --for=condition=Ready --timeout=-1s
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward project-microservices-udacity 5000:80
+
+# Step 5:
+# See the output of app running into pods
+kubectl logs project-microservices-udacity  
